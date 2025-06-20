09e11f791bacc8cc737cbff69b91f6a118b39fb9
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-06-20 20:58:13 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,6 +2,15 @@ if GetCurrentResourceName() ~= "trplr_radio" then
     return print("^6Changing the resource's name wont't let the resource start, ^1" .. GetCurrentResourceName() .. "^0 > ^2 trplr_radio ^7")
 end
 
+CreateThread(function()
+   print("  ____              _                      _        ")
+   print(" | __ )   _   _    | |       ___   _ __   (_) __  __")
+   print(" |  _ \\  | | | |   | |     / _ \\  | '_ \\  | | \\\\/ /")
+   print(" | |_) | | |_| |   | |___  | __/  | | | | | |  >  < ")
+   print(" |____/   \\__, |   |_____| \\___|  |_| |_| |_| /_/\\\\ ")
+   print("          |___/                                     ")
+)
+
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("radio", function(source)
